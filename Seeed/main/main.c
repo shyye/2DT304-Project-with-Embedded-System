85869d27d34a32d7d68918e8552cc1d6653d39dc
Mattias Stålgren <ms225kw@student.lnu.se>
2024-02-21 16:19:45 +0100

    Add long range mode

--- Seeed/main/main.c
+++ Seeed/main/main.c
@@ -90,11 +90,12 @@ static void app_wifi_init()
     esp_event_loop_create_default();
 
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-
+    
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
     ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
     ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+    ESP_ERROR_CHECK(esp_wifi_set_protocol(WIFI_IF_STA, WIFI_PROTOCOL_LR));
     ESP_ERROR_CHECK(esp_wifi_start());
 }
 
