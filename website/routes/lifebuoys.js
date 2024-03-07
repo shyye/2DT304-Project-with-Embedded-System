33bff125877c8e743031b5bf5523749f4ea068f6
shyye <emma.art.dev@gmail.com>
2024-03-07 09:33:39 +0100

    Update routes/lifebuoy with search/filter test

--- website/routes/lifebuoys.js
+++ website/routes/lifebuoys.js
@@ -10,10 +10,18 @@ const data = require('../lifebuoys.json')
 
 // Display all lifebuoys
 router.get('/', async (req, res) => {
+
+    let filters = {}
+    if (req.query.search != null && req.query.search !== '') {
+        filters.search = new RegExp(req.query.search, 'i')
+    }
     try {
 
         // TODO: Search for lifebuoys
-        const lifebuoys = await Lifebuoy.find({})
+        // https://www.youtube.com/watch?v=esy4nRuShl8&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=7
+        // At 29:13 in the video
+        // and the next video
+        const lifebuoys = await Lifebuoy.find(filters)
 
         // Convert Zone value to name
         res.status(200).render('lifebuoys/index', { 
@@ -28,7 +36,6 @@ router.get('/', async (req, res) => {
 
 // Display form to create new lifebuoy
 router.get('/new', async (req, res) => {
-    // res.send('Create lifebuoy') TODO:
     res.render('lifebuoys/new', { lifebuoy: new Lifebuoy() })
 })
 
@@ -60,7 +67,7 @@ router.post('/create', async (req, res) => {
         // })
   
         // res.status(201).json({ message: "Lifebuoy created", lifebuoy: lifebuoy })
-        // res.redirect(`lifebuoys/${lifebuoy.id}`)
+        // res.redirect(`lifebuoys/${lifebuoy.id}`) TODO: Redricit in video
         res.redirect('/lifebuoys')
         
     } catch (error) {
@@ -83,16 +90,55 @@ router.post('/create', async (req, res) => {
 
 
 // TODO: 
-router.route('/:id')
-    .get((req, res) => {
-        res.send(`Get lifebuoy with id: ${req.params.id}`)
-    })
-    .put((req, res) => {
-        res.send(`Get lifebuoy with id: ${req.params.id}`)
-    })
-    .delete((req, res) => {
-        res.send(`Get lifebuoy with id: ${req.params.id}`)
-    })
+// Video #5 https://youtu.be/UIf1Lh9OZ-k?si=xF2h25RqaUsT-jbp
+//  Update
+router.get('/:id', (req, res) => {
+    res.send(`Show lifebuoy with id: ${req.params.id}`)
+})
+
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const lifebuoy = await Lifebuoy.findById(req.params.id)
+        res.render('lifebuoys/edit', { lifebuoy: lifebuoy })
+    } catch (error) {
+        res.redirect('/lifebuoys')
+    }   
+})
+
+// Update
+router.put('/:id', async (req, res) => {
+    let lifebuoy
+    try {
+        lifebuoy = await Lifebuoy.findById(req.params.id)
+        lifebuoy.deviceId = req.body.deviceId,
+        lifebuoy.zone = req.body.zone,
+        lifebuoy.latitude = req.body.latitude,
+        lifebuoy.longitude = req.body.longitude,
+        lifebuoy.manufactureDate = req.body.manufactureDate,
+        lifebuoy.expirationDate = req.body.expirationDate,
+        lifebuoy.status = req.body.status
+        await lifebuoy.save()
+
+        // res.redirect(`/lifebuoys/${lifebuoy.id}`)
+        res.redirect(`/lifebuoys`)
+        
+    } catch (error) {
+        if (lifebuoy == null) {
+            res.redirect('/')
+        } else {
+            res.render('lifebuoys/new', {
+                lifebuoy: req.body,
+                errorMessage: 'Error updating lifebuoy'
+            })
+        }             
+    }
+    // res.send(`Update lifebuoy with id: ${req.params.id}`)
+})
+
+// Delete
+router.delete('/:id', (req, res) => {
+    res.send(`Delete lifebuoy with id: ${req.params.id}`)
+})
 
 // Temp function
 // TODO: Test different error scenarios, example lat/long
